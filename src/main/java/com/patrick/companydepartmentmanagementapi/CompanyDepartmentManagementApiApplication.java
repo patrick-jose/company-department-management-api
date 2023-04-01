410f5c0046b22200e5e70db487f2f6b76907482e
patric-jose <patric-jose@hotmail.com>
2023-03-31 23:54:25 -0300

    Initial commit

---
+++ src/main/java/com/patrick/companydepartmentmanagementapi/CompanyDepartmentManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.patrick.companydepartmentmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyDepartmentManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyDepartmentManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patrick/companydepartmentmanagementapi/CompanyDepartmentManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrick.companydepartmentmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyDepartmentManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
